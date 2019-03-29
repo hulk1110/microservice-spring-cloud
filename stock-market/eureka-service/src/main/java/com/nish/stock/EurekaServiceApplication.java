2781e5569aa4de19170daf648c4d3136ee50e216
Nishant <739833@cognizant.com>
2019-03-29 11:50:30 +0530

    setting up api gateway

--- stock-market/eureka-service/src/main/java/com/nish/stock/EurekaServiceApplication.java
+++ stock-market/eureka-service/src/main/java/com/nish/stock/EurekaServiceApplication.java
@@ -3,7 +3,8 @@ package com.nish.stock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+@EnableZuulProxy
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServiceApplication {
